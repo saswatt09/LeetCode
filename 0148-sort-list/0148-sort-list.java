3d1dd1537d03028ea11fa93c970dc4f85614e374
saswatt09 <148701184+saswatt09@users.noreply.github.com>
2025-01-27 10:47:47 +0530

    Time: 13 ms (26.89%), Space: 55.1 MB (93.49%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -1,49 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-    // Merge Sort Implementation
-    public ListNode getMid(ListNode head){
-        ListNode slow=head, fast=head.next;
-        while(fast != null && fast.next != null){
-            fast = fast.next.next;
-            slow = slow.next;
-        }
-        return slow;
-    }
-    public ListNode merge(ListNode left, ListNode right){
-        ListNode dummy = new ListNode();
-        ListNode tail = dummy;
-
-        while(left != null && right != null){
-            if(left.val < right.val){
-                tail.next = left;
-                left = left.next;
-            }else{
-                tail.next = right;
-                right = right.next;
-            }
-            tail = tail.next;
-        }
-        if(left != null){
-            tail.next = left;
-        }
-        if(right != null){
-            tail.next = right;
-        }
-        return dummy.next;
-    }
     public ListNode sortList(ListNode head) {
-        if(head == null || head.next == null){
+        if (head == null || head.next == null) {
             return head;
         }
 
-        // Split the list in 2 halfs
-        ListNode left = head;
-        ListNode right = getMid(head);
-        ListNode tmp = right.next;
-        right.next = null;
-        right = tmp;
+        List<Integer> values = new ArrayList<>();
+        ListNode current = head;
+        while (current != null) {
+            values.add(current.val);
+            current = current.next;
+        }
 
-        left = sortList(left);
-        right = sortList(right);
-        return merge(left, right);
+        Collections.sort(values);
+        current = head;
+        for (int value : values) {
+            current.val = value;
+            current = current.next;
+        }
+
+        return head; 
     }
 }
