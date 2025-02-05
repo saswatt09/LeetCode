ce608c20fb03840de68f5fdf52063f3bbfc42e41
saswatt09 <148701184+saswatt09@users.noreply.github.com>
2025-02-05 21:55:29 +0530

    Time: 1726 ms (5%), Space: 0B (100%) - LeetHub

---
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+SELECT product_id # return the product_id col
+FROM Products # table w/ info 
+WHERE low_fats = "Y" AND recyclable = "Y"; # low fats and recyclable need to Y
