a9a6733f2813a832ee461ab1bec547eb86a59742
Saswat Samantaray <148701184+saswatt09@users.noreply.github.com>
2025-02-17 00:28:31 +0530

    Time: 39 ms (21.69%), Space: 55 MB (36.91%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.java
+++ 0355-design-twitter/0355-design-twitter.java
@@ -1,44 +1,81 @@
 class Twitter {
-
-    private Map<Integer, Set<Integer>> userMap; // userId & followeeIds
-    private List<Pair<Integer, Integer>> tweetList; // List containining pair of userId and tweetId
-
-    public Twitter() {
-        userMap = new HashMap<>();
-        tweetList = new ArrayList<>();
-    }
+  int count;
+  HashMap<Integer, List<int[]>> tweetMap;
+  HashMap<Integer, HashSet<Integer>> followerMap;
     
-    public void postTweet(int userId, int tweetId) {
-
-        userMap.computeIfAbsent(userId, k -> new HashSet<>());
-        tweetList.add(new Pair(userId, tweetId));
-    }
+  public Twitter() {
+    count = 0;
+    tweetMap = new HashMap<>();
+    followerMap = new HashMap<>();
+  }
     
-    public List<Integer> getNewsFeed(int userId) {
-        List<Integer> list = new ArrayList<>();
-        if(!userMap.containsKey(userId)){
-            return list;
-        }
+  public void postTweet(int userId, int tweetId) {
+    tweetMap.computeIfAbsent(userId,
+      k -> new ArrayList<>()
+    );
 
-        int index = tweetList.size() - 1;
+    tweetMap.computeIfPresent(userId, (k, v) -> {
+      v.add(new int[]{count, tweetId});
+      return v;
+    });
 
-        while(list.size() < 10 && index >= 0) {
-            Pair<Integer, Integer> pair = tweetList.get(index--);
-            if(pair.getKey() == userId || userMap.get(userId).contains(pair.getKey())) {
-                list.add(pair.getValue());
-            }
-        }
-        
-        return list;
-    }
+    ++count;
+  }
     
-    public void follow(int followerId, int followeeId) {
-        userMap.computeIfAbsent(followerId, k -> new HashSet<>()).add(followeeId);
+  public List<Integer> getNewsFeed(int userId) {
+    List<Integer> res = new ArrayList<>();
+
+    PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> 
+      Integer.compare(b[0], a[0])
+    );
+
+    followerMap.computeIfAbsent(userId,
+      k -> new HashSet<>()
+    );
+
+    followerMap.get(userId).add(userId);
+
+    followerMap.get(userId).forEach((followeeId) -> {
+      if (tweetMap.containsKey(followeeId)) {
+        int i = tweetMap.get(followeeId).size() - 1;
+        int[] tweet = tweetMap.get(followeeId).get(i);
+        pq.offer(new int[]{tweet[0], tweet[1], 
+          followeeId, --i});  
+      }
+    });
+
+    while (!pq.isEmpty() && res.size() < 10) {
+      int[] data = pq.poll();
+      res.add(data[1]);
+
+      if (data[3] >= 0) {
+        int[] tweet = tweetMap.get(data[2]).get(data[3]);
+        pq.offer(new int[]{tweet[0], tweet[1], 
+          data[2], --data[3]});
+      }
     }
+
+    return res;
+  }
     
-    public void unfollow(int followerId, int followeeId) {
-        userMap.get(followerId).remove(followeeId);
+  public void follow(int followerId, int followeeId) {
+    followerMap.computeIfAbsent(followerId, 
+      k -> new HashSet<>());
+
+    followerMap.computeIfPresent(followerId, (k, v) -> {
+      v.add(followeeId);
+      return v;
+    }); 
+  }
+    
+  public void unfollow(int followerId, int followeeId) {
+    HashSet<Integer> set = followerMap.computeIfAbsent(followerId, 
+      k -> new HashSet<>());
+
+    if (set.contains(followeeId)) {
+      set.remove(followeeId);
     }
+  }
 }
 
 /**
