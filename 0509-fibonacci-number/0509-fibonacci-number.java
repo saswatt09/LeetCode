b477f265c38bb977d8b2a55d4620db32d83c6aaf
Saswat Samantaray <148701184+saswatt09@users.noreply.github.com>
2025-03-17 09:59:23 +0530

    Time: 90 ms (12.01%), Space: 44.2 MB (6.26%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,6 +1,16 @@
 class Solution {
     public int fib(int n) {
-        if(n<=1) return n;
-        return fib(n-1) + fib(n-2);
+        // if(n<=1) return n;
+        // return fib(n-1) + fib(n-2);
+
+        Map<Integer, Integer> cache = new HashMap<>();
+        if (n<=1) return n;
+        if(cache.containsKey(n)){
+            return cache.get(n);
+        }
+        int result = fib(n-1) + fib(n-2);
+        cache.put(n,result);
+
+        return result;
     }
 }
