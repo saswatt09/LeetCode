012d6fae4df157708ac27bfdcd9a2b1dadcca6aa
Saswat Samantaray <148701184+saswatt09@users.noreply.github.com>
2025-07-19 00:47:22 +0530

    Time: 2 ms (99.13%), Space: 42.5 MB (40.62%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -5,7 +5,7 @@ class Solution {
         int count = 0;
 
         for (char c : s.toCharArray()){
-            if(c = '('){
+            if(c == '('){
                 if (count> 0){
                     sb.append(c);
                 }
